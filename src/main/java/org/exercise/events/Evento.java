2f03f88da284e29ca1df25a43bb597483b891cf3
Davide Cangiano <cangiano.0497@gmail.com>
2023-09-05 11:04:23 +0200

    Initial commit

---
+++ src/main/java/org/exercise/events/Evento.java
@@ -0,0 +1,33 @@
+package org.exercise.events;
+/*
+La consegna è di creare una classe Evento che abbia le seguenti proprietà:
+● titolo
+● data
+● numero di posti in totale
+● numero di posti prenotati
+Quando si istanzia un nuovo evento questi attributi devono essere tutti valorizzati nel costruttore , tranne posti prenotati che va inizializzato a 0.
+Inserire il controllo che la data non sia già passata e che il numero di posti totali sia positivo. In caso contrario sollevare opportune eccezioni.
+Aggiungere metodi getter e setter in modo che:
+● titolo sia in lettura e in scrittura
+● data sia in lettura e scrittura
+● numero di posti totale sia solo in lettura
+● numero di posti prenotati sia solo in lettura
+Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni:
+1. prenota : aggiunge un certo numero di posti prenotati. Se l’evento è già passato o non ha posti disponibili deve sollevare un’eccezione.
+2. disdici : riduce di un certo numero i posti prenotati. Se l’evento è già passato o non ci sono prenotazioni deve sollevare un’eccezione.
+3. l’override del metodo toString() in modo che venga restituita una stringa contenente: data formattata - titolo
+Aggiungete eventuali metodi (public e private) che vi aiutino a svolgere le funzioni richieste.
+ */
+public class Evento {
+    // ATTRIBUTI
+
+
+    // COSTRUTTORI
+
+
+    // GETTER E SETTER
+
+
+
+    // METODI
+}
---
+++ src/main/java/org/exercise/events/Main.java
@@ -0,0 +1,35 @@
+package org.exercise.events;
+
+import java.util.Scanner;
+
+/*
+1. Creare una classe Main di test, in cui si chiede all’utente di inserire un nuovo evento con tutti i parametri.
+2. Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni vuole fare e provare ad effettuarle, implementando opportuni controlli e gestendo eventuali eccezioni.
+3. Stampare a video il numero di posti prenotati e quelli disponibili
+4. Chiedere all’utente se e quanti posti vuole disdire
+5. Provare ad effettuare le disdette, implementando opportuni controlli e gestendo eventuali eccezioni
+6. Stampare a video il numero di posti prenotati e quelli disponibili
+ */
+public class Main {
+    public static void main(String[] args) {
+        // inizializzo lo scanner
+        Scanner input = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // chiudo lo scanner
+        input.close();
+    }
+}
